9559cf2e80b698b22bc7bff80c819a344394f170
0030481621011 <0030481621011@LTI6305.lti.fatecso.edu.br>
2018-05-07 11:57:57 -0300

    nova classe

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Professor.java
@@ -0,0 +1,29 @@
+package sistema.modelos;
+
+public class Professor {
+
+	private int codigo;
+	private String nome;
+	private String formacao;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getFormacao() {
+		return formacao;
+	}
+	public void setFormacao(String formacao) {
+		this.formacao = formacao;
+	}
+	
+	
+}
